a4bc37ddabb41e52360e955b53e1a6f36e97e4bf
Zain Suleiman <109575838+PROMCz11@users.noreply.github.com>
2025-04-18 22:35:43 +0300

    Removed guards to allow people with no accounts to receive updates

--- src/updates/updates.controller.ts
+++ src/updates/updates.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { UpdatesService } from './updates.service';
 import { AccountAuthGuard } from 'src/common/guards/account.guard';
 
-@UseGuards(AccountAuthGuard)
+// @UseGuards(AccountAuthGuard)
 @Controller('updates')
 export class UpdatesController {
   constructor(private readonly updatesService: UpdatesService) {}
